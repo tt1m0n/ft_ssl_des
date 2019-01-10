8a00eaaaabbafd2fe17acb8fd74bec378d4b3749
CyFIR Developer <developer@CyFIRs-Mac-4.local>
2019-01-10 08:54:43 -0800

    start des

---
+++ src/ft_md5.c
@@ -0,0 +1,122 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_md5.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omakovsk <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/02 09:41:48 by omakovsk          #+#    #+#             */
+/*   Updated: 2018/09/02 09:41:56 by omakovsk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft/libft.h"
+#include "ft_md5.h"
+
+void				md5_body_cycle(t_uint *tmp, t_md5_info *ctx,\
+									const t_uchar *ptr)
+{
+	t_uint	saved_tmp[4];
+
+	saved_tmp[0] = tmp[0];
+	saved_tmp[1] = tmp[1];
+	saved_tmp[2] = tmp[2];
+	saved_tmp[3] = tmp[3];
+	md5_round1(tmp, ctx, ptr);
+	md5_round2(tmp, ctx);
+	md5_round3(tmp, ctx);
+	md5_round4(tmp, ctx);
+	tmp[0] += saved_tmp[0];
+	tmp[1] += saved_tmp[1];
+	tmp[2] += saved_tmp[2];
+	tmp[3] += saved_tmp[3];
+}
+
+static const void	*body(t_md5_info *ctx, const void *data, unsigned long size)
+{
+	const t_uchar	*ptr;
+	t_uint			tmp[4];
+
+	ptr = (const t_uchar*)data;
+	tmp[0] = ctx->a;
+	tmp[1] = ctx->b;
+	tmp[2] = ctx->c;
+	tmp[3] = ctx->d;
+	while (size)
+	{
+		md5_body_cycle(tmp, ctx, ptr);
+		ptr += 64;
+		size -= 64;
+	}
+	ctx->a = tmp[0];
+	ctx->b = tmp[1];
+	ctx->c = tmp[2];
+	ctx->d = tmp[3];
+	return (ptr);
+}
+
+void				init_var(t_uint *saved_lo, t_ulong *used,\
+							t_md5_info *md5, t_ulong size)
+{
+	*saved_lo = md5->lo;
+	if ((md5->lo = (*saved_lo + size) & 0x1fffffff) < *saved_lo)
+		md5->hi++;
+	md5->hi += size >> 29;
+	*used = *saved_lo & 0x3f;
+}
+
+void				md5_update_words(t_md5_info *md5, const void *data,\
+										unsigned long size)
+{
+	t_uint	saved_lo;
+	t_ulong	used;
+	t_ulong	available;
+
+	init_var(&saved_lo, &used, md5, size);
+	if (used)
+	{
+		available = 64 - used;
+		if (size < available)
+		{
+			ft_memcpy(&md5->buffer[used], data, size);
+			return ;
+		}
+		ft_memcpy(&md5->buffer[used], data, available);
+		data = (const t_uchar*)data + available;
+		size -= available;
+		body(md5, md5->buffer, 64);
+	}
+	if (size >= 64)
+	{
+		data = body(md5, data, size & ~(t_ulong)0x3f);
+		size &= 0x3f;
+	}
+	ft_memcpy(md5->buffer, data, size);
+}
+
+void				md5_final(unsigned char *result, t_md5_info *ctx)
+{
+	t_ulong used;
+	t_ulong available;
+
+	used = ctx->lo & 0x3f;
+	ctx->buffer[used++] = 0x80;
+	available = 64 - used;
+	if (available < 8)
+	{
+		ft_memset(&ctx->buffer[used], 0, available);
+		body(ctx, ctx->buffer, 64);
+		used = 0;
+		available = 64;
+	}
+	ft_memset(&ctx->buffer[used], 0, available - 8);
+	ctx->lo <<= 3;
+	out(&ctx->buffer[56], ctx->lo);
+	out(&ctx->buffer[60], ctx->hi);
+	body(ctx, ctx->buffer, 64);
+	out(&result[0], ctx->a);
+	out(&result[4], ctx->b);
+	out(&result[8], ctx->c);
+	out(&result[12], ctx->d);
+	ft_memset(ctx, 0, sizeof(*ctx));
+}
---
+++ src/main.c
@@ -0,0 +1,123 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omakovsk <omakovsk@student.unit.ua>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/25 15:59:00 by omakovsk          #+#    #+#             */
+/*   Updated: 2018/09/02 14:56:31 by omakovsk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_global.h"
+#include "ft_md5.h"
+#include "ft_sha.h"
+#include "ft_parse_and_read.h"
+
+t_hash_ptr	find_hash_type(char *hash)
+{
+	int i;
+
+	i = START_VALUE;
+	while (g_hash_name[i] != 0)
+	{
+		if (ft_strcmp(g_hash_name[i], hash) == 0)
+			return (g_hash_function[i]);
+		i++;
+	}
+	return (0);
+}
+
+int			printf_no_flag_error(t_hash_ptr hash_type)
+{
+	ft_printf("usage: ");
+	if (hash_type == md5)
+		ft_printf("md5 ");
+	if (hash_type == sha224)
+		ft_printf("sha224 ");
+	if (hash_type == sha256)
+		ft_printf("sha256 ");
+	if (hash_type == sha384)
+		ft_printf("sha384 ");
+	if (hash_type == sha512)
+		ft_printf("sha512 ");
+	ft_printf("[-pqrtx] [-s string] [files ...]\n");
+	return (1);
+}
+
+int			print_error(t_hash_ptr hash_type,\
+						t_error_type error_type, char *filename)
+{
+	if (error_type == no_flag)
+		return (printf_no_flag_error(hash_type));
+	if (hash_type == md5)
+		ft_printf("md5: ");
+	if (hash_type == sha224)
+		ft_printf("sha224: ");
+	if (hash_type == sha256)
+		ft_printf("sha256: ");
+	if (hash_type == sha384)
+		ft_printf("sha384: ");
+	if (hash_type == sha512)
+		ft_printf("sha512: ");
+	if (error_type == no_file)
+		ft_printf("%s: No such file or directory\n", filename);
+	if (error_type == try_open_dir)
+		ft_printf("%s: Is a directory\n", filename);
+	if (error_type == error_command)
+	{
+		ft_printf("Message Digest commands:\n");
+		ft_printf("md5\nsha224\nsha256\nsha384\nsha512\n");
+	}
+	return (1);
+}
+
+char		*take_text_from_file(int fd)
+{
+	char	*need_to_hash;
+	char	*tmp_buffer;
+	char	*ptr_to_free;
+
+	need_to_hash = ft_memalloc(1);
+	tmp_buffer = ft_memalloc(BUFFER_SIZE);
+	while (read(fd, tmp_buffer, BUFFER_SIZE - 1))
+	{
+		ptr_to_free = need_to_hash;
+		need_to_hash = ft_strjoin(need_to_hash, tmp_buffer);
+		free(ptr_to_free);
+		ft_memset(tmp_buffer, 0, BUFFER_SIZE);
+	}
+	free(tmp_buffer);
+	if (fd)
+		close(fd);
+	return (need_to_hash);
+}
+
+int			main(int argc, char **argv)
+{
+	t_hash_info info;
+
+	info.flags = 0;
+	info.hashptr = 0;
+	argv[argc] = NULL;
+	if (argc == 1)
+		ft_printf(USAGE_STRING);
+	else
+	{
+		if (!(info.hashptr = find_hash_type(argv[1])))
+			return (print_error(0, error_command, *argv));
+		argv = argv + 2;
+		if (*argv == NULL)
+			return (read_hash_info(&info, STDIN, 1));
+		while (*argv && (*argv)[0] == '-')
+		{
+			if (parse_flags(&info, &argv) == UNKNOWN_FLAG)
+				return (print_error(info.hashptr, no_flag, *argv));
+			argv++;
+		}
+		while (*argv)
+			read_hash_info(&info, *argv++, 1);
+	}
+	return (0);
+}
